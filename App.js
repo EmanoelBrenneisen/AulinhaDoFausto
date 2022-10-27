ee5b3ced2eb88fc4d2800eb9097d47f441ead2d4
emanoelBrenneisen <emanoelbrenneisen@gmail.com>
2022-10-27 16:29:57 -0300

    Mudei Tmb

--- App.js
+++ App.js
@@ -1,31 +1,12 @@
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { StyleSheet, Text, View } from 'react-native';
+import { RootNavigation } from './src/Navigation';
 
-export function HomeScreen() {
- return (
-    <View style={styles.container}>
-      <Text>Pessoal sou eu de novo!</Text>
-    </View>
-  );
-}
 
-const Stack = createNativeStackNavigator();
 export default function App(){
   return(
     <NavigationContainer>
-      <Stack.Navigator>
-      <Stack.Screen name={'Home'} component={HomeScreen}/>
-      </Stack.Navigator>
+    <RootNavigation></RootNavigation>
     </NavigationContainer>
   )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
